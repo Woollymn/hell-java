b21ad7a6ef54c82360803a46ccb7f0662ac22124
woollymn <woolymn@gmail.com>
2016-07-27 10:45:34 +0900

    IntelliJ 빌드 설정 변경 & 캐릭터셋 UTF-8로 설정하는 필터 추가

---
+++ servlets/hell-board/src/com/hellBoard/filter/CharacterSetFilter.java
@@ -0,0 +1,38 @@
+package com.hellBoard.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by hkkang on 2016. 7. 27..
+ */
+public class CharacterSetFilter implements Filter {
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.encoding = filterConfig.getInitParameter("requestEncoding");
+
+        if (this.encoding == null) {
+            this.encoding = "UTF-8";
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+
+        if(servletRequest.getCharacterEncoding() == null) {
+            servletRequest.setCharacterEncoding(this.encoding);
+        }
+
+        servletResponse.setCharacterEncoding(this.encoding);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {}
+}
