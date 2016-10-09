5a32357f2dd39b5809b79da3d080b8907b4a87ae
woollymn <woollymn@gmail.com>
2016-10-09 23:55:44 +0900

    example, jpashop 추가

---
+++ example/src/net/woollymn/Main.java
@@ -0,0 +1,13 @@
+package net.woollymn;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
+
+enum Elvis {
+    INSTANCE;
+
+    public void leaveTheBuilding() {}
+}
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,14 +1,13 @@
-package jpabook.model.entity;
+package jpabook.jpashop.domain;
 
+import jpabook.jpashop.domain.item.Item;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "ORDER_ITEM")
 @Data
-@NoArgsConstructor
 public class OrderItem {
 
     @Id
@@ -16,11 +15,11 @@ public class OrderItem {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
